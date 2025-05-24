cc09dc149d647b2452b17e9fd5d314433c06d8e7
rigr <richard.grauss@gmail.com>
2025-05-24 10:09:15 +0200

    Update tusb_config.h

--- include/tusb_config.h
+++ include/tusb_config.h
@@ -1,21 +1,32 @@
 #ifndef _TUSB_CONFIG_H_
 #define _TUSB_CONFIG_H_
 
-#define CFG_TUSB_RHPORT0_MODE   (OPT_MODE_HOST | OPT_MODE_DEVICE)
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+#define CFG_TUSB_RHPORT0_MODE   (OPT_MODE_HOST | OPT_MODE_DEVICE)
 #define CFG_TUSB_OS             OPT_OS_PICO
 #define CFG_TUSB_MCU            OPT_MCU_RP2040
 
+// Device Configuration
 #define CFG_TUD_ENABLED         1
-#define CFG_TUH_ENABLED         1
-
-// Device configuration
-#define CFG_TUD_MIDI           1
+#define CFG_TUD_MIDI            1
 #define CFG_TUD_MIDI_RX_BUFSIZE 64
 #define CFG_TUD_MIDI_TX_BUFSIZE 64
 
-// Host configuration
-#define CFG_TUH_MIDI           1
-#define CFG_TUH_DEVICE_MAX     2
+// Host Configuration
+#define CFG_TUH_ENABLED         1
+#define CFG_TUH_MIDI            1
+#define CFG_TUH_DEVICE_MAX      2
+
+// Descriptor callbacks
+#define CFG_TUD_DESC_DEVICE_CB   1
+#define CFG_TUD_DESC_CONFIG_CB   1
+#define CFG_TUD_DESC_STRING_CB   1
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
