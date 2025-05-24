02658faa764c5cbde987f62837664ccda289c98a
rigr <richard.grauss@gmail.com>
2025-05-24 10:20:09 +0200

    Update usb_descriptors.h

--- include/usb_descriptors.h
+++ include/usb_descriptors.h
@@ -3,13 +3,17 @@
 
 #include "tusb.h"
 
-// Device descriptor callback
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Callbacks
 uint8_t const * tud_descriptor_device_cb(void);
-
-// Configuration descriptor callback
 uint8_t const * tud_descriptor_configuration_cb(uint8_t index);
-
-// String descriptor callback
 uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
