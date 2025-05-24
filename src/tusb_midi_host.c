b567fad9c4e1d9417bda1d76166f8b77c5dfa026
rigr <richard.grauss@gmail.com>
2025-05-24 13:31:14 +0200

    Update tusb_midi_host.c

--- src/tusb_midi_host.c
+++ src/tusb_midi_host.c
@@ -1,11 +1,21 @@
 #include "tusb.h"
 #include "usb_midi_host.h"
 
-// Implement missing host MIDI functions
+// Complete implementation of missing MIDI host functions
 uint32_t tuh_midi_stream_read(uint8_t dev_addr, uint8_t *cable_num, void *buffer, uint16_t bufsize) {
-    return tuh_midi_stream_read_ext(dev_addr, 0, cable_num, buffer, bufsize);
+    TU_VERIFY(tuh_midi_mounted(dev_addr), 0);
+    
+    uint8_t const ep_addr = usb_host_devices[dev_addr-1].in_ep;
+    TU_VERIFY(ep_addr, 0);
+    
+    return tuh_xfer(dev_addr, ep_addr, buffer, bufsize, true);
 }
 
 bool tuh_midi_stream_write(uint8_t dev_addr, uint8_t cable_num, void const * buffer, uint16_t bufsize) {
-    return tuh_midi_stream_write_ext(dev_addr, 0, cable_num, buffer, bufsize);
+    TU_VERIFY(tuh_midi_mounted(dev_addr), false);
+    
+    uint8_t const ep_addr = usb_host_devices[dev_addr-1].out_ep;
+    TU_VERIFY(ep_addr, false);
+    
+    return tuh_xfer(dev_addr, ep_addr, (void*)buffer, bufsize, false) == XFER_RESULT_SUCCESS;
 }
